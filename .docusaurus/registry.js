3a01e63af1c3f0ebf69980f0e42c3be0faa0f9fd
heath-zingly <139074861+mphdigital@users.noreply.github.com>
2024-02-03 01:51:41 -0500

    updated package

--- .docusaurus/registry.js
+++ .docusaurus/registry.js
@@ -1,168 +1,20 @@
 export default {
-  "__comp---site-src-pages-index-jsc-4-f-f99": [
-    () =>
-      import(
-        /* webpackChunkName: "__comp---site-src-pages-index-jsc-4-f-f99" */ "@site/src/pages/index.js"
-      ),
-    "@site/src/pages/index.js",
-    require.resolveWeak("@site/src/pages/index.js"),
-  ],
-  "__comp---theme-debug-config-23-a-2ff": [
-    () =>
-      import(
-        /* webpackChunkName: "__comp---theme-debug-config-23-a-2ff" */ "@theme/DebugConfig"
-      ),
-    "@theme/DebugConfig",
-    require.resolveWeak("@theme/DebugConfig"),
-  ],
-  "__comp---theme-debug-contentba-8-ce7": [
-    () =>
-      import(
-        /* webpackChunkName: "__comp---theme-debug-contentba-8-ce7" */ "@theme/DebugContent"
-      ),
-    "@theme/DebugContent",
-    require.resolveWeak("@theme/DebugContent"),
-  ],
-  "__comp---theme-debug-global-dataede-0fa": [
-    () =>
-      import(
-        /* webpackChunkName: "__comp---theme-debug-global-dataede-0fa" */ "@theme/DebugGlobalData"
-      ),
-    "@theme/DebugGlobalData",
-    require.resolveWeak("@theme/DebugGlobalData"),
-  ],
-  "__comp---theme-debug-registry-679-501": [
-    () =>
-      import(
-        /* webpackChunkName: "__comp---theme-debug-registry-679-501" */ "@theme/DebugRegistry"
-      ),
-    "@theme/DebugRegistry",
-    require.resolveWeak("@theme/DebugRegistry"),
-  ],
-  "__comp---theme-debug-routes-946-699": [
-    () =>
-      import(
-        /* webpackChunkName: "__comp---theme-debug-routes-946-699" */ "@theme/DebugRoutes"
-      ),
-    "@theme/DebugRoutes",
-    require.resolveWeak("@theme/DebugRoutes"),
-  ],
-  "__comp---theme-debug-site-metadata-68-e-3d4": [
-    () =>
-      import(
-        /* webpackChunkName: "__comp---theme-debug-site-metadata-68-e-3d4" */ "@theme/DebugSiteMetadata"
-      ),
-    "@theme/DebugSiteMetadata",
-    require.resolveWeak("@theme/DebugSiteMetadata"),
-  ],
-  "__comp---theme-doc-item-178-a40": [
-    () =>
-      import(
-        /* webpackChunkName: "__comp---theme-doc-item-178-a40" */ "@theme/DocItem"
-      ),
-    "@theme/DocItem",
-    require.resolveWeak("@theme/DocItem"),
-  ],
-  "__comp---theme-doc-roota-94-67a": [
-    () =>
-      import(
-        /* webpackChunkName: "__comp---theme-doc-roota-94-67a" */ "@theme/DocRoot"
-      ),
-    "@theme/DocRoot",
-    require.resolveWeak("@theme/DocRoot"),
-  ],
-  "__comp---theme-doc-version-roota-7-b-5de": [
-    () =>
-      import(
-        /* webpackChunkName: "__comp---theme-doc-version-roota-7-b-5de" */ "@theme/DocVersionRoot"
-      ),
-    "@theme/DocVersionRoot",
-    require.resolveWeak("@theme/DocVersionRoot"),
-  ],
-  "__comp---theme-docs-root-5-e-9-0b6": [
-    () =>
-      import(
-        /* webpackChunkName: "__comp---theme-docs-root-5-e-9-0b6" */ "@theme/DocsRoot"
-      ),
-    "@theme/DocsRoot",
-    require.resolveWeak("@theme/DocsRoot"),
-  ],
-  "allContent---docusaurus-debug-content-246-9aa": [
-    () =>
-      import(
-        /* webpackChunkName: "allContent---docusaurus-debug-content-246-9aa" */ "~debug/default/docusaurus-debug-all-content-673.json"
-      ),
-    "~debug/default/docusaurus-debug-all-content-673.json",
-    require.resolveWeak("~debug/default/docusaurus-debug-all-content-673.json"),
-  ],
-  "config---5-e-9-4f3": [
-    () =>
-      import(
-        /* webpackChunkName: "config---5-e-9-4f3" */ "@generated/docusaurus.config"
-      ),
-    "@generated/docusaurus.config",
-    require.resolveWeak("@generated/docusaurus.config"),
-  ],
-  "content---business-docs-intro-958-d73": [
-    () =>
-      import(
-        /* webpackChunkName: "content---business-docs-intro-958-d73" */ "@site/docs/business-docs/intro.md"
-      ),
-    "@site/docs/business-docs/intro.md",
-    require.resolveWeak("@site/docs/business-docs/intro.md"),
-  ],
-  "content---docs-technical-docs-introa-24-e30": [
-    () =>
-      import(
-        /* webpackChunkName: "content---docs-technical-docs-introa-24-e30" */ "@site/docs/technical-docs/intro.md"
-      ),
-    "@site/docs/technical-docs/intro.md",
-    require.resolveWeak("@site/docs/technical-docs/intro.md"),
-  ],
-  "content---docs-technical-docs-markdown-features-7-ef-bfe": [
-    () =>
-      import(
-        /* webpackChunkName: "content---docs-technical-docs-markdown-features-7-ef-bfe" */ "@site/docs/technical-docs/markdown-features.mdx"
-      ),
-    "@site/docs/technical-docs/markdown-features.mdx",
-    require.resolveWeak("@site/docs/technical-docs/markdown-features.mdx"),
-  ],
-  "plugin---docsc-20-112": [
-    () =>
-      import(
-        /* webpackChunkName: "plugin---docsc-20-112" */ "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-content-docs/default/plugin-route-context-module-100.json"
-      ),
-    "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-content-docs/default/plugin-route-context-module-100.json",
-    require.resolveWeak(
-      "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-content-docs/default/plugin-route-context-module-100.json"
-    ),
-  ],
-  "plugin---docusaurus-debugbe-5-e99": [
-    () =>
-      import(
-        /* webpackChunkName: "plugin---docusaurus-debugbe-5-e99" */ "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-debug/default/plugin-route-context-module-100.json"
-      ),
-    "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-debug/default/plugin-route-context-module-100.json",
-    require.resolveWeak(
-      "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-debug/default/plugin-route-context-module-100.json"
-    ),
-  ],
-  "plugin---markdown-paged-9-f-db2": [
-    () =>
-      import(
-        /* webpackChunkName: "plugin---markdown-paged-9-f-db2" */ "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-content-pages/default/plugin-route-context-module-100.json"
-      ),
-    "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-content-pages/default/plugin-route-context-module-100.json",
-    require.resolveWeak(
-      "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-content-pages/default/plugin-route-context-module-100.json"
-    ),
-  ],
-  "version---docs-935-398": [
-    () =>
-      import(
-        /* webpackChunkName: "version---docs-935-398" */ "~docs/default/version-current-metadata-prop-751.json"
-      ),
-    "~docs/default/version-current-metadata-prop-751.json",
-    require.resolveWeak("~docs/default/version-current-metadata-prop-751.json"),
-  ],
-};
+  "__comp---site-src-pages-index-jsc-4-f-f99": [() => import(/* webpackChunkName: "__comp---site-src-pages-index-jsc-4-f-f99" */ "@site/src/pages/index.js"), "@site/src/pages/index.js", require.resolveWeak("@site/src/pages/index.js")],
+  "__comp---theme-debug-config-23-a-2ff": [() => import(/* webpackChunkName: "__comp---theme-debug-config-23-a-2ff" */ "@theme/DebugConfig"), "@theme/DebugConfig", require.resolveWeak("@theme/DebugConfig")],
+  "__comp---theme-debug-contentba-8-ce7": [() => import(/* webpackChunkName: "__comp---theme-debug-contentba-8-ce7" */ "@theme/DebugContent"), "@theme/DebugContent", require.resolveWeak("@theme/DebugContent")],
+  "__comp---theme-debug-global-dataede-0fa": [() => import(/* webpackChunkName: "__comp---theme-debug-global-dataede-0fa" */ "@theme/DebugGlobalData"), "@theme/DebugGlobalData", require.resolveWeak("@theme/DebugGlobalData")],
+  "__comp---theme-debug-registry-679-501": [() => import(/* webpackChunkName: "__comp---theme-debug-registry-679-501" */ "@theme/DebugRegistry"), "@theme/DebugRegistry", require.resolveWeak("@theme/DebugRegistry")],
+  "__comp---theme-debug-routes-946-699": [() => import(/* webpackChunkName: "__comp---theme-debug-routes-946-699" */ "@theme/DebugRoutes"), "@theme/DebugRoutes", require.resolveWeak("@theme/DebugRoutes")],
+  "__comp---theme-debug-site-metadata-68-e-3d4": [() => import(/* webpackChunkName: "__comp---theme-debug-site-metadata-68-e-3d4" */ "@theme/DebugSiteMetadata"), "@theme/DebugSiteMetadata", require.resolveWeak("@theme/DebugSiteMetadata")],
+  "__comp---theme-doc-item-178-a40": [() => import(/* webpackChunkName: "__comp---theme-doc-item-178-a40" */ "@theme/DocItem"), "@theme/DocItem", require.resolveWeak("@theme/DocItem")],
+  "__comp---theme-doc-roota-94-67a": [() => import(/* webpackChunkName: "__comp---theme-doc-roota-94-67a" */ "@theme/DocRoot"), "@theme/DocRoot", require.resolveWeak("@theme/DocRoot")],
+  "__comp---theme-doc-version-roota-7-b-5de": [() => import(/* webpackChunkName: "__comp---theme-doc-version-roota-7-b-5de" */ "@theme/DocVersionRoot"), "@theme/DocVersionRoot", require.resolveWeak("@theme/DocVersionRoot")],
+  "__comp---theme-docs-root-5-e-9-0b6": [() => import(/* webpackChunkName: "__comp---theme-docs-root-5-e-9-0b6" */ "@theme/DocsRoot"), "@theme/DocsRoot", require.resolveWeak("@theme/DocsRoot")],
+  "allContent---docusaurus-debug-content-246-9aa": [() => import(/* webpackChunkName: "allContent---docusaurus-debug-content-246-9aa" */ "~debug/default/docusaurus-debug-all-content-673.json"), "~debug/default/docusaurus-debug-all-content-673.json", require.resolveWeak("~debug/default/docusaurus-debug-all-content-673.json")],
+  "config---5-e-9-4f3": [() => import(/* webpackChunkName: "config---5-e-9-4f3" */ "@generated/docusaurus.config"), "@generated/docusaurus.config", require.resolveWeak("@generated/docusaurus.config")],
+  "content---business-docs-intro-958-d73": [() => import(/* webpackChunkName: "content---business-docs-intro-958-d73" */ "@site/docs/business-docs/intro.md"), "@site/docs/business-docs/intro.md", require.resolveWeak("@site/docs/business-docs/intro.md")],
+  "content---technical-docs-introa-24-b94": [() => import(/* webpackChunkName: "content---technical-docs-introa-24-b94" */ "@site/docs/technical-docs/intro.md"), "@site/docs/technical-docs/intro.md", require.resolveWeak("@site/docs/technical-docs/intro.md")],
+  "plugin---c-20-ea4": [() => import(/* webpackChunkName: "plugin---c-20-ea4" */ "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-content-docs/default/plugin-route-context-module-100.json"), "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-content-docs/default/plugin-route-context-module-100.json", require.resolveWeak("/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-content-docs/default/plugin-route-context-module-100.json")],
+  "plugin---d-9-f-768": [() => import(/* webpackChunkName: "plugin---d-9-f-768" */ "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-content-pages/default/plugin-route-context-module-100.json"), "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-content-pages/default/plugin-route-context-module-100.json", require.resolveWeak("/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-content-pages/default/plugin-route-context-module-100.json")],
+  "plugin---docusaurus-debugbe-5-e99": [() => import(/* webpackChunkName: "plugin---docusaurus-debugbe-5-e99" */ "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-debug/default/plugin-route-context-module-100.json"), "/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-debug/default/plugin-route-context-module-100.json", require.resolveWeak("/Users/heathbodell/Documents/Work/MPH.DIGITAL/mph-digital.github.io/.docusaurus/docusaurus-plugin-debug/default/plugin-route-context-module-100.json")],
+  "version---935-fcf": [() => import(/* webpackChunkName: "version---935-fcf" */ "~docs/default/version-current-metadata-prop-751.json"), "~docs/default/version-current-metadata-prop-751.json", require.resolveWeak("~docs/default/version-current-metadata-prop-751.json")],};
