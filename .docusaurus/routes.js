e63d716e4e9c75af7c4936543fa6313c8473ce1c
heath-zingly <139074861+mphdigital@users.noreply.github.com>
2024-02-03 01:02:30 -0500

    updates

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -1,85 +1,88 @@
-import React from 'react';
-import ComponentCreator from '@docusaurus/ComponentCreator';
+import React from "react";
+import ComponentCreator from "@docusaurus/ComponentCreator";
 
 export default [
   {
-    path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug', '2e3'),
-    exact: true
+    path: "/__docusaurus/debug",
+    component: ComponentCreator("/__docusaurus/debug", "2e3"),
+    exact: true,
   },
   {
-    path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config', 'bf2'),
-    exact: true
+    path: "/__docusaurus/debug/config",
+    component: ComponentCreator("/__docusaurus/debug/config", "bf2"),
+    exact: true,
   },
   {
-    path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content', '8b6'),
-    exact: true
+    path: "/__docusaurus/debug/content",
+    component: ComponentCreator("/__docusaurus/debug/content", "8b6"),
+    exact: true,
   },
   {
-    path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData', '79f'),
-    exact: true
+    path: "/__docusaurus/debug/globalData",
+    component: ComponentCreator("/__docusaurus/debug/globalData", "79f"),
+    exact: true,
   },
   {
-    path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata', '33c'),
-    exact: true
+    path: "/__docusaurus/debug/metadata",
+    component: ComponentCreator("/__docusaurus/debug/metadata", "33c"),
+    exact: true,
   },
   {
-    path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry', 'c47'),
-    exact: true
+    path: "/__docusaurus/debug/registry",
+    component: ComponentCreator("/__docusaurus/debug/registry", "c47"),
+    exact: true,
   },
   {
-    path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes', 'e1d'),
-    exact: true
+    path: "/__docusaurus/debug/routes",
+    component: ComponentCreator("/__docusaurus/debug/routes", "e1d"),
+    exact: true,
   },
   {
-    path: '/',
-    component: ComponentCreator('/', 'ee2'),
-    exact: true
+    path: "/",
+    component: ComponentCreator("/", "ee2"),
+    exact: true,
   },
   {
-    path: '/',
-    component: ComponentCreator('/', '154'),
+    path: "/",
+    component: ComponentCreator("/", "154"),
     routes: [
       {
-        path: '/',
-        component: ComponentCreator('/', 'f89'),
+        path: "/",
+        component: ComponentCreator("/", "f89"),
         routes: [
           {
-            path: '/',
-            component: ComponentCreator('/', '43f'),
+            path: "/",
+            component: ComponentCreator("/", "43f"),
             routes: [
               {
-                path: '/business-docs/intro',
-                component: ComponentCreator('/business-docs/intro', '62e'),
+                path: "/business-docs/intro",
+                component: ComponentCreator("/business-docs/intro", "62e"),
                 exact: true,
-                sidebar: "busDocsSidebar"
+                sidebar: "busDocsSidebar",
               },
               {
-                path: '/technical-docs/intro',
-                component: ComponentCreator('/technical-docs/intro', '1c6'),
+                path: "/technical-docs/intro",
+                component: ComponentCreator("/technical-docs/intro", "1c6"),
                 exact: true,
-                sidebar: "techDocsSidebar"
+                sidebar: "techDocsSidebar",
               },
               {
-                path: '/technical-docs/markdown-features',
-                component: ComponentCreator('/technical-docs/markdown-features', '1da'),
+                path: "/technical-docs/markdown-features",
+                component: ComponentCreator(
+                  "/technical-docs/markdown-features",
+                  "1da"
+                ),
                 exact: true,
-                sidebar: "techDocsSidebar"
-              }
-            ]
-          }
-        ]
-      }
-    ]
+                sidebar: "techDocsSidebar",
+              },
+            ],
+          },
+        ],
+      },
+    ],
   },
   {
-    path: '*',
-    component: ComponentCreator('*'),
+    path: "*",
+    component: ComponentCreator("*"),
   },
 ];
